328cb155a8e109e648f62340d406c30c1ead1ad4
Scott Tolinski <scott.tolinski@gmail.com>
2019-09-03 14:07:39 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import GlobalVars from "./GlobalVars.svelte";
+
+  export let name;
+</script>
+
+<style>
+  h1 {
+    font-size: 4rem;
+    color: var(--teal);
+  }
+</style>
+
+<GlobalVars />
+<h1>Hello {name}!</h1>
